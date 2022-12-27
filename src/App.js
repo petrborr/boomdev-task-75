935539dae83dc5bcd338bd2d46d5b2de9c481e40
petrborr <petrborr@gmail.com>
2022-12-27 17:03:42 +0200

    fix: buttons to save/remove note from local storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [note, setNote] = useState("")
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea className="textarea is-large"
+             onChange={(e)=>setNote(e.target.value)} placeholder="Notes..." 
+             defaultValue={localStorage.getItem("note")} />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active"
+        onClick={() => localStorage.setItem("note", note)}>Save</button>
+        <button className="button is-large"
+        onClick={()=> {
+          localStorage.removeItem("note")
+          document.querySelector(".textarea").value = ""
+        }}>Clear</button>
       </div>
     </div>
   );
